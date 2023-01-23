31f30349a3ac111c220419758432533228bc00bc
Rahul Khandelwal <khandelwalr213@gmail.com>
2023-01-23 17:39:39 +0530

    first commit

---
+++ models/usersSchema.js
@@ -0,0 +1,63 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const userSchema = new mongoose.Schema({
+  fname: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+
+  lname: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw Error("Invalid email");
+      }
+    },
+  },
+
+  mobile: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 10,
+    maxlength: 10,
+  },
+
+  gender: {
+    type: String,
+    required: true,
+  },
+
+  status: {
+    type: String,
+    required: true,
+  },
+
+  profile: {
+    type: String,
+    required: true,
+  },
+
+  location: {
+    type: String,
+    required: true,
+  },
+
+  datecreated: Date ,
+  dateUpdated: Date
+});
+
+// models 
+
+const users = mongoose.model('users ' , userSchema);
+
+module.exports = users; 
